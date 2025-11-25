af4770061c4c962707d8f889ca5c1896a6028e2b
HENNZO DA SILVA <57859273810@imaccommand008.local>
2025-11-25 11:53:54 -0300

    Exercício 08 finalizado

---
+++ Loop/exercício09.js
@@ -0,0 +1,13 @@
+// 9. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let num = parseInt(prompt("DIgite 0:"))
+
+while (num !== 0){
+num = parseInt(prompt("Digite 0:"))
+}
+console.log(num)
+
+if(n == 0){
+    window.alert("0 selecionado!")
+}
