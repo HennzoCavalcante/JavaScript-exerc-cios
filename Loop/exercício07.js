949f072d6c2446a1993ee9f264b87a307950698b
HENNZO DA SILVA <57859273810@imaccommand008.local>
2025-11-25 11:36:03 -0300

    Exercício 07 finalizado

---
+++ Loop/exercício07.js
@@ -0,0 +1,10 @@
+// 7. Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let num = parseInt(prompt("Insira um número:"))
+
+do {
+    console.log("Contagem regressiva" , num)
+    num--
+    
+} while (num >= 0);
