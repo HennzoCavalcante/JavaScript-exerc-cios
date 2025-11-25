b9d1d69bc42666772aeeca6ff5a3621a7698c10c
HENNZO DA SILVA <57859273810@imaccommand008.local>
2025-11-25 13:33:25 -0300

    Exercício 06 finalizado

--- Loop/exercício06.js
+++ Loop/exercício06.js
@@ -1,13 +1,23 @@
 // 6. Calcular a soma dos números de 1 a 100 utilizando a estrutura
 // while.
 
-let soma = parseInt(prompt("Escolha um número"))
-let num = 1
-let resultado = 0
+// let soma = parseInt(prompt("Escolha um número"))
+// let num = 1
+// let resultado = 0
 
-while (num <= soma) {
-    resultado = resultado + num
+// while (num <= soma) {
+//     soma = soma + num
+//     num++
+// }
+
+// console.log("Soma total: ", resultado); 
+
+let num = 1
+let soma = 0 
+while (num <= 100){
+    soma = soma + num
+
+    console.log(soma)
     num++
 }
-
-console.log("Soma total: ", resultado); 
+console.log("Soma total: " + soma)
